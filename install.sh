fdbbb27b986502aa7fa8ef14b903142250da64aa
vaibhavhirani <vaibhavhirani@gmail.com>
2023-02-26 11:26:12 +0530

    Trivial but works for me

--- install.sh
+++ install.sh
@@ -1,2 +1,27 @@
+echo "Tell me what to install now 
+1. gh 
+2. az cli 
+"
+read tool_no
+if [ $tool_no -eq  "1" ]
+then
+echo "Installing Github CLI \n"
 type -p curl >/dev/null || sudo apt install curl -y
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo "deb [arch=amd64 signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null && sudo apt update && sudo apt install gh -y
+elif [ $tool_no -eq "2" ]
+then
+echo "Installing AZ ClI \n"
+sudo apt remove azure-cli -y && sudo apt autoremove -y
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install azure-cli
+fi
